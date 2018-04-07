ccecd64c302fef56f4ae606afe084aee446d333a
levanchon <vanchonlee1@gmail.com>
2018-04-07 14:35:53 +0700

    LSB c#

---
+++ LSB/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace LSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageImput = new System.Windows.Forms.PictureBox();
+            this.imageOutput = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnImageInput = new System.Windows.Forms.Button();
+            this.btnLSB = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.lblKq = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageImput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageImput
+            // 
+            this.imageImput.Location = new System.Drawing.Point(12, 54);
+            this.imageImput.Name = "imageImput";
+            this.imageImput.Size = new System.Drawing.Size(157, 96);
+            this.imageImput.TabIndex = 0;
+            this.imageImput.TabStop = false;
+            // 
+            // imageOutput
+            // 
+            this.imageOutput.Location = new System.Drawing.Point(238, 54);
+            this.imageOutput.Name = "imageOutput";
+            this.imageOutput.Size = new System.Drawing.Size(157, 96);
+            this.imageOutput.TabIndex = 1;
+            this.imageOutput.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnImageInput
+            // 
+            this.btnImageInput.Location = new System.Drawing.Point(12, 169);
+            this.btnImageInput.Name = "btnImageInput";
+            this.btnImageInput.Size = new System.Drawing.Size(75, 23);
+            this.btnImageInput.TabIndex = 2;
+            this.btnImageInput.Text = "select ";
+            this.btnImageInput.UseVisualStyleBackColor = true;
+            this.btnImageInput.Click += new System.EventHandler(this.btnImageInput_Click);
+            // 
+            // btnLSB
+            // 
+            this.btnLSB.Location = new System.Drawing.Point(12, 227);
+            this.btnLSB.Name = "btnLSB";
+            this.btnLSB.Size = new System.Drawing.Size(383, 81);
+            this.btnLSB.TabIndex = 3;
+            this.btnLSB.Text = "LSB";
+            this.btnLSB.UseVisualStyleBackColor = true;
+            this.btnLSB.Click += new System.EventHandler(this.btnLSB_Click);
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(238, 169);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(75, 23);
+            this.btnExtract.TabIndex = 4;
+            this.btnExtract.Text = "extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // lblKq
+            // 
+            this.lblKq.AutoSize = true;
+            this.lblKq.Location = new System.Drawing.Point(235, 204);
+            this.lblKq.Name = "lblKq";
+            this.lblKq.Size = new System.Drawing.Size(35, 13);
+            this.lblKq.TabIndex = 5;
+            this.lblKq.Text = "label1";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 201);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(157, 20);
+            this.txtInput.TabIndex = 6;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "LÊ VĂN CHƠN - N14DCAT076 -- lỚP D14CQAT01_N";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblKq);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnLSB);
+            this.Controls.Add(this.btnImageInput);
+            this.Controls.Add(this.imageOutput);
+            this.Controls.Add(this.imageImput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageImput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageImput;
+        private System.Windows.Forms.PictureBox imageOutput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnImageInput;
+        private System.Windows.Forms.Button btnLSB;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.Label lblKq;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LSB/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string pSelectImage;
+        private void btnImageInput_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+            pSelectImage = this.openFileDialog1.FileName;
+            this.imageImput.Image = Image.FromFile(pSelectImage);
+            imageImput.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private byte insertBit(byte input, byte x)
+        {
+            byte temp = input;
+            int r = (int)input;
+            if (x == 0x00 && ((byte)temp & 0x01) == 0x01)
+            {
+                r = (int)temp - 1;
+            }
+            else if (x == 0x01 && ((byte)temp & 0x01) == 0x00)
+            {
+                r = (int)temp + 1;
+            }
+            return (byte)r;
+        }
+
+        private void btnLSB_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmapImage = new Bitmap(pSelectImage);
+            List<byte> listByteBitmap = new List<byte>();
+            string txt = this.txtInput.Text;
+            List<char> arrchar = txt.ToCharArray().ToList();
+            listByteBitmap = getArrByteBitMap(bitmapImage);
+            arrchar.Add('|');
+
+            //chen
+            int dem = 0;
+            for(int i = 0; i < listByteBitmap.Count; i=i+8)
+            {
+                if (dem < arrchar.Count)
+                {
+                    char cTemp = arrchar[dem++];
+                    listByteBitmap[i+0] = insertBit(listByteBitmap[i+0], (byte)((cTemp >> 7) & 0x01));//0
+                    listByteBitmap[i+1] = insertBit(listByteBitmap[i+1], (byte)((cTemp >> 6) & 0x01));//1
+                    listByteBitmap[i+2] = insertBit(listByteBitmap[i+2], (byte)((cTemp >> 5) & 0x01));//2
+                    listByteBitmap[i+3] = insertBit(listByteBitmap[i+3], (byte)((cTemp >> 4) & 0x01));//3
+                    listByteBitmap[i+4] = insertBit(listByteBitmap[i+4], (byte)((cTemp >> 3) & 0x01));//4
+                    listByteBitmap[i+5] = insertBit(listByteBitmap[i+5], (byte)((cTemp >> 2) & 0x01));//5
+                    listByteBitmap[i+6] = insertBit(listByteBitmap[i+6], (byte)((cTemp >> 1) & 0x01));//6
+                    listByteBitmap[i+7] = insertBit(listByteBitmap[i+7], (byte)((cTemp >> 0) & 0x01));//7
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            
+
+            Bitmap bitNew = new Bitmap(bitmapImage.Width, bitmapImage.Height);
+            int dem1 = 0;
+            for(int i = 0; i < bitNew.Height; i++)
+            {
+                for(int j = 0; j < bitNew.Width; j++)
+                {
+                    Color colorTem = new Color();
+                    byte r = listByteBitmap[dem1++];
+                    byte g = listByteBitmap[dem1++];
+                    byte b = listByteBitmap[dem1++];
+                    colorTem = Color.FromArgb(r, g, b);
+                    bitNew.SetPixel(j, i, colorTem);
+                }
+            }
+            this.imageOutput.Image = (Image)bitNew;
+            imageOutput.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //save file
+            
+        }
+
+        private List<byte> getArrByteBitMap(Bitmap bitmapImage)
+        {
+            List<byte> listByteBitmap = new List<byte>();
+            for (int height = 0; height < bitmapImage.Height; height++)
+            {
+                for (int width = 0; width < bitmapImage.Width; width++)
+                {
+                    Color color_xy = new Color();
+                    color_xy = bitmapImage.GetPixel(width, height);
+                    byte g = color_xy.G;
+                    byte r = color_xy.R;
+                    byte b = color_xy.B;
+                    listByteBitmap.Add(r);
+                    listByteBitmap.Add(g);
+                    listByteBitmap.Add(b);
+                }
+            }
+            return listByteBitmap;
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            List<byte> listByteBitmap = new List<byte>();
+            Bitmap bitmapImage = new Bitmap(this.imageOutput.Image);
+            listByteBitmap = getArrByteBitMap(bitmapImage);
+            List<char> arrchar = new List<char>();
+            int i = 0;
+            byte cTemp = 0x00;
+            while (cTemp!='|')
+            {
+                cTemp = 0x00;
+                for(int j = 0; j < 8; j++)
+                {
+                    cTemp = (byte)(cTemp + (byte)(listByteBitmap[i + j] & 0x01));//lay bit thu 0
+                    if (j < 7)
+                    {
+                        cTemp = (byte)(cTemp << 1);
+                    }
+                }
+                
+                arrchar.Add((char)cTemp);
+                
+                i = i+8;
+            }
+            arrchar.Remove('|');
+            this.lblKq.Text = string.Join("", arrchar);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = this.saveFileDialog1;
+            sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                this.imageOutput.Image.Save(sfd.FileName, format);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+            pSelectImage = this.openFileDialog1.FileName;
+            this.imageOutput.Image = Image.FromFile(pSelectImage);
+            imageOutput.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
